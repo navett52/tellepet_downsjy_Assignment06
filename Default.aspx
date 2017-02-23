7fd7f5845bf1ec38eb9cf68e8a0a1558caf02a0b
navett52 <evantellep@yahoo.com>
2017-02-22 20:44:03 -0500

    Gahhhh!!!!

---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:DropDownList ID="ddLoyaltyNumbers" runat="server"></asp:DropDownList> <%-- No dependencies --%>
+        <asp:Calendar ID="calDoteOfTrans" runat="server"></asp:Calendar> 
+        <asp:TextBox ID="txtTimeOfTrans" runat="server"></asp:TextBox> <%-- Use Regex to validate time format --%>
+        <asp:DropDownList ID="ddTransType" runat="server"></asp:DropDownList> <%-- No dependencies --%>
+        <asp:DropDownList ID="ddStores" runat="server"></asp:DropDownList> <%-- No dependencies --%>
+        <asp:DropDownList ID="ddEmpl" runat="server"></asp:DropDownList> <%-- Dependencies: Stores --%>
+        <asp:DropDownList ID="ddProduct" runat="server"></asp:DropDownList> <%-- Dependencies: Stores --%>
+        <asp:TextBox ID="txtQty" runat="server"></asp:TextBox> <%-- Validate to only accept numbers --%>
+        <asp:DropDownList ID="ddPricePerSellableUnitAsMarked" runat="server"></asp:DropDownList> <%-- Dependencies: Product --%>
+        <asp:DropDownList ID="ddPricePerSellableUnitToCustomer" runat="server"></asp:DropDownList> <%-- Dependencies: Product --%>
+        <asp:TextBox ID="txtTransComment" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtTransDetailComment" runat="server"></asp:TextBox>
+        <asp:TextBox ID="txtCouponDetailID" runat="server"></asp:TextBox> <%-- Validate to only accept numbers --%>
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Making the objects that interact with the DB
+    private static System.Data.SqlClient.SqlConnection conn;
+    private static SqlCommand comm;
+    private static SqlDataReader reader;
+
+    /// <summary>
+    /// Populate the dropdown and populate the available enchantments for the first dropdown item on the page load
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        {
+            openConnection();
+        }
+    }
+
+    /// <summary>
+    /// Opening the database connection
+    /// </summary>
+    public void openConnection()
+    {
+        System.Configuration.ConnectionStringSettings strConn;
+        strConn = ReadConnectionString();
+        // Console.WriteLine(strConn.ConnectionString);
+
+        conn = new System.Data.SqlClient.SqlConnection(strConn.ConnectionString);
+
+        // This could go wrong in so many ways...
+        try
+        {
+            conn.Open();
+        }
+        catch (Exception ex)
+        {
+            // Miserable error handling...
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    /** Read the connection string from the web.config file. 
+      * This is a much more secure way to store sensitive information. Don't hard-code connection information in the source code.
+      * Adapted from http://msdn.microsoft.com/en-us/library/ms178411.aspx
+      */
+    private System.Configuration.ConnectionStringSettings ReadConnectionString()
+    {
+        String strPath;
+        strPath = HttpContext.Current.Request.ApplicationPath + "/web.config";
+        System.Configuration.Configuration rootWebConfig =
+            System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration(strPath);
+
+        System.Configuration.ConnectionStringSettings connString = null;
+        if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+        {
+            connString = rootWebConfig.ConnectionStrings.ConnectionStrings["GroceryStoreSimulatorPopulator"];
+        }
+        return connString;
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        Regex checktime = new Regex(@"^(?:[01]?[0-9]|2[0-3]):[0-5][0-9]$"); //Ref: http://stackoverflow.com/questions/884848/regular-expression-to-validate-valid-time
+        if (!checktime.IsMatch(txtTimeOfTrans.Text))
+        {
+            Response.Write("The time entered is not in the correct format!");
+        } 
+    }
+}
